7379d579410a05c4d7e43656c2f3b800353b47dd
Subendu Das <66836529+iamsubendu@users.noreply.github.com>
2022-01-10 17:48:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// module.exports = function () {
+//   return "Hello";
+// };
+
+// for multiple functions
+module.exports = {
+  sayHello: function () {
+    return "Hello";
+  },
+  addNumbers: function (x, y) {
+    return x + y;
+  },
+};
